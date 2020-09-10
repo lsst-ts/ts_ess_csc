42fd113a86faa419651aff0930da99f0580555e5
forrestgk <garryknight1@hotmail.com>
2020-09-10 07:13:07 -0700

    Multi-channel temperature instument
    Instrument reader for SEL temperature instuments that read a configurable number of channels.
    Updates to comply with Python coding and documentation standards (more work to be done).
    First acquisition of data from instrument is ignored to avoid initial exception report for received partial line. Exceptions are still generated for subsequent receptions of incorrect line length.

--- python/lsst/ts/ess/ESS_start.py
+++ python/lsst/ts/ess/ESS_start.py
@@ -1,15 +1,15 @@
 # *******************************************************************************
-# File:         ESS_start.py
-# Date:         2020-07-24
+# File:         ess_start.py
+# Date:         2020-09-08
 # Author:       Garry Knight - Garry Knight, LLC
 #
 # Description:  LSST Environmental Sensors System (ESS) startup.
 #               This script should be run immediately following system start.
 #
 # *******************************************************************************
-import ESS_temperature_4ch_reader
+import ess_temperature_reader
 
-SCRIPT_NAME = 'ESS_start'
+SCRIPT_NAME = 'ess_start'
 
 '''
 Initiate and run 4-channel temperature instrument thread using ttyUSB0.
@@ -17,9 +17,23 @@ This assumes that the instrument to be read is enumerated as ttyUSB0. This is
 guaranteed if the instrument is the only virtual com port (VCP) device connected
 to USB ports.
 '''
-ESS_Temp4 = ESS_temperature_4ch_reader.ESS_Temperature_4ch('/dev/ttyUSB0', 19200, 1.5)
-ESS_Temp4.serial_open()
-instrThr = ESS_temperature_4ch_reader.instrumentThread('thrd', ESS_Temp4)
-instrThr.run()
-ESS_temp4.serial_close()
+ESS_TEMPERATURE_OBJECT_NAME: str = "ess_temp"
+ESS_TEMPERATURE_CHANNELS = 6
+ESS_TEMPERATURE_SERIAL_PORT: str = "/dev/ttyUSB0"
+ESS_TEMPERATURE_SERIAL_BAUD: int = 19200
+ESS_TEMPERATURE_SERIAL_TIMEOUT: float = 1.5
+
+ess_temp = ess_temperature_reader.ESS_Temperature(
+        ESS_TEMPERATURE_OBJECT_NAME,
+        ESS_TEMPERATURE_CHANNELS,
+        ESS_TEMPERATURE_SERIAL_PORT,
+        ESS_TEMPERATURE_SERIAL_BAUD, 
+        ESS_TEMPERATURE_SERIAL_TIMEOUT)
+
+ess_temp._serial_open()
+try:
+    ess_temp.read_instrument()
+except:
+    pass
+instr_thr = ess_temperature_reader.InstrumentThread("temp_loop1", ess_temp)
 
