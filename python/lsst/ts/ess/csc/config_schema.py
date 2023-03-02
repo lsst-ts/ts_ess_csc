c95a608c4dd736f55f035cc64a633bc91c6c12ea
Wouter van Reeven <wvanreeven@lsst.org>
2023-03-02 15:09:00 +0100

    Add location to lightning sensors telemetry.

--- python/lsst/ts/ess/csc/config_schema.py
+++ python/lsst/ts/ess/csc/config_schema.py
@@ -28,8 +28,8 @@ CONFIG_SCHEMA = yaml.safe_load(
 $schema: http://json-schema.org/draft-07/schema#
 $id: https://github.com/lsst-ts/ts_ess/blob/main/python/lsst/ts/ess/csc/config_schema.py
 # title must end with one or more spaces followed by the schema version, which must begin with "v"
-title: ESS v3
-description: Schema for ESS configuration
+title: ESS v4
+description: Schema for ESS configuration.
 type: object
 properties:
   instances:
