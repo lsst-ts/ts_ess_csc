115c236bfc99fca9e2eda0505fda196dfaecf05c
Russell Owen <rowen@uw.edu>
2023-04-26 07:40:15 -0700

    get_circular_mean_and_std_dev: log the angles, as well

--- python/lsst/ts/ess/csc/accumulator/utils.py
+++ python/lsst/ts/ess/csc/accumulator/utils.py
@@ -74,7 +74,8 @@ def get_circular_mean_and_std_dev(
         if log is not None:
             log.warning(
                 "Could not compute circular std dev: "
-                f"{complex_sum=!r}; {math.log(abs(complex_sum))=!r}"
+                f"{complex_sum=!r}; {math.log(abs(complex_sum))=!r}; "
+                f"{angles=}"
             )
         circular_std = math.nan
     # For ts_xml the value is cast to int, so nan or inf are illegal.
