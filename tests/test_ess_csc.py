811921f9d6e63c1efcdba89b6e3eaf75ffc5933b
Wouter van Reeven <wouter@van.reeven.nl>
2025-05-28 20:58:32 +0200

    Start using rewritten DataClient lifecycle.

--- tests/test_ess_csc.py
+++ tests/test_ess_csc.py
@@ -25,7 +25,6 @@ import logging
 import pathlib
 import types
 import unittest
-from unittest import mock
 
 import astropy.units as u
 import numpy as np
@@ -34,6 +33,7 @@ from astropy.units import misc
 from lsst.ts import salobj, tcpip, utils
 from lsst.ts.ess import common, csc
 from lsst.ts.ess.common.test_utils import MockTestTools
+from lsst.ts.salobj.topics import ReadTopic
 from lsst.ts.xml.enums.ESS import ErrorCode
 
 STD_TIMEOUT = 10  # standard command timeout (sec)
@@ -50,7 +50,7 @@ TOO_LONG_WAIT_TIME = 12
 NUM_ALL_SENSORS = 5
 # The number os seconds to wait for a summary state change. This needs to be
 # set to a sufficiently high number so the timeout tests don't fail.
-STATE_TIMEOUT = 60
+STATE_TIMEOUT = 10
 
 # Config override string to avoid duplication.
 ALL_SENSORS_YAML = "test_all_sensors.yaml"
@@ -172,7 +172,7 @@ class CscTestCase(salobj.BaseCscTestCase, unittest.IsolatedAsyncioTestCase):
 
     async def next_data(
         self,
-        topics: list[salobj.topics.ReadTopic],
+        topics: list[ReadTopic],
         sensor_name: str,
         timeout: float = STD_TIMEOUT,
     ) -> dict[str, salobj.BaseMsgType]:
@@ -207,7 +207,7 @@ class CscTestCase(salobj.BaseCscTestCase, unittest.IsolatedAsyncioTestCase):
         )
 
     async def _next_data_impl(
-        self, topics: list[salobj.topics.ReadTopic], sensor_name: str
+        self, topics: list[ReadTopic], sensor_name: str
     ) -> dict[str, salobj.BaseMsgType]:
         """Implementation of next_data, without the timeout."""
         topics_data: dict[str, salobj.BaseMsgType] = dict()
@@ -225,7 +225,7 @@ class CscTestCase(salobj.BaseCscTestCase, unittest.IsolatedAsyncioTestCase):
 
     async def _loop_ver_topics(
         self,
-        topics: list[salobj.topics.ReadTopic],
+        topics: list[ReadTopic],
         sensor_name: str,
         topics_data: dict[str, salobj.BaseMsgType],
     ) -> None:
@@ -462,7 +462,6 @@ class CscTestCase(salobj.BaseCscTestCase, unittest.IsolatedAsyncioTestCase):
             assert len(self.csc.data_clients) == NUM_ALL_SENSORS
             for data_client in self.csc.data_clients:
                 assert isinstance(data_client, common.data_client.ControllerDataClient)
-                assert data_client.socket_server.connected
 
             await self.validate_telemetry()
 
@@ -471,36 +470,12 @@ class CscTestCase(salobj.BaseCscTestCase, unittest.IsolatedAsyncioTestCase):
             await salobj.set_summary_state(
                 remote=self.remote, state=salobj.State.DISABLED
             )
+
+            # Give time to all DataClients to stop.
+            await asyncio.sleep(2.0)
+
             for data_client in self.csc.data_clients:
-                assert not data_client.socket_server.connected
-
-    async def test_rpi_data_client_loses_connecton(self) -> None:
-        """The CSC should fault when a ControllerDataClient loses its
-        connection to the server.
-        """
-        async with self.make_csc(
-            initial_state=salobj.State.ENABLED,
-            config_dir=TEST_CONFIG_DIR,
-            simulation_mode=1,
-            override=ALL_SENSORS_YAML,
-        ):
-            await self.assert_next_summary_state(
-                salobj.State.ENABLED, timeout=STATE_TIMEOUT
-            )
-            await self.assert_next_sample(topic=self.remote.evt_errorCode, errorCode=0)
-            assert len(self.csc.data_clients) == NUM_ALL_SENSORS
-            for data_client in self.csc.data_clients:
-                assert data_client.socket_server.connected
-
-            # Disconnect one of the mock servers
-            await self.csc.data_clients[1].socket_server.close()
-
-            await self.assert_next_summary_state(
-                salobj.State.FAULT, timeout=STATE_TIMEOUT
-            )
-            await self.assert_next_sample(
-                topic=self.remote.evt_errorCode, errorCode=ErrorCode.ConnectionLost
-            )
+                assert not data_client.connected
 
     async def test_rpi_data_client_cannot_connect(self) -> None:
         """The CSC should fault if a ControllerDataClient cannot connect
@@ -525,65 +500,20 @@ class CscTestCase(salobj.BaseCscTestCase, unittest.IsolatedAsyncioTestCase):
             for data_client in self.csc.data_clients:
                 assert data_client.enable_socket_server
             self.csc.data_clients[1].enable_socket_server = False
-            with salobj.assertRaisesAckError():
-                await self.remote.cmd_enable.start(timeout=STD_TIMEOUT)
-            await self.assert_next_summary_state(
-                salobj.State.FAULT, timeout=STATE_TIMEOUT
-            )
-            await self.assert_next_sample(
-                topic=self.remote.evt_errorCode, errorCode=ErrorCode.ConnectionFailed
-            )
-
-    @mock.patch(
-        "lsst.ts.ess.common.data_client.controller_data_client.COMMUNICATE_TIMEOUT",
-        COMMUNICATE_TIMEOUT,
-    )
-    async def test_rpi_data_client_timeout(self) -> None:
-        async with self.make_csc(
-            initial_state=salobj.State.ENABLED,
-            config_dir=TEST_CONFIG_DIR,
-            simulation_mode=1,
-        ):
+            await self.remote.cmd_enable.start(timeout=STD_TIMEOUT)
             await self.assert_next_summary_state(
                 salobj.State.ENABLED, timeout=STATE_TIMEOUT
             )
-            await self.assert_next_sample(topic=self.remote.evt_errorCode, errorCode=0)
-            assert len(self.csc.data_clients) == 1
-            for data_client in self.csc.data_clients:
-                assert data_client.socket_server.connected
-
-            await self.validate_telemetry()
-
-            # Set the wait time for the device to SHORT_WAIT_TIME, which is a
-            # higher value than the default (see
-            # lsst.ts.ess.common.device.MockDevice).
-            common.device.MockDevice.telemetry_interval = LONG_WAIT_TIME
-
-            # Here the default wait time still is used.
-            await self.validate_telemetry()
-
-            # Here SHORT_WAIT_TIME is used. This should NOT time out.
-            await self.validate_telemetry()
-
-            # Set the wait time for the device to LONG_WAIT_TIME, which is an
-            # even higher value than the default (see
-            # lsst.ts.ess.common.device.MockDevice).
-            common.device.MockDevice.telemetry_interval = TOO_LONG_WAIT_TIME
-
-            # Here SHORT_WAIT_TIME still is used.
-            await self.validate_telemetry()
-
-            # Here LONG_WAIT_TIME is used. This should time out.
             await self.assert_next_summary_state(
                 salobj.State.FAULT, timeout=STATE_TIMEOUT
             )
             await self.assert_next_sample(
-                topic=self.remote.evt_errorCode, errorCode=ErrorCode.ConnectionLost
+                topic=self.remote.evt_errorCode, errorCode=ErrorCode.RunFailed
             )
 
     async def test_restart_csc(self) -> None:
         """The CSC should NOT fault when the CSC is set to STANDBY and then to
-        ENABLED again..
+        ENABLED again.
         """
         # Start the MockServer for manual control.
         await self.start_socket_server()
@@ -612,57 +542,6 @@ class CscTestCase(salobj.BaseCscTestCase, unittest.IsolatedAsyncioTestCase):
 
             while self.socket_server.command_handler._started:
                 await asyncio.sleep(0.01)
-
-            await salobj.set_summary_state(
-                remote=self.remote, state=salobj.State.ENABLED
-            )
-            await self.assert_next_summary_state(
-                salobj.State.DISABLED, timeout=STATE_TIMEOUT
-            )
-            await self.assert_next_summary_state(
-                salobj.State.ENABLED, timeout=STATE_TIMEOUT
-            )
-
-        # Stop the MockServer to clean up after ourselves.
-        await self.stop_socket_server()
-
-    async def test_rpi_data_client_loses_connection(self) -> None:
-        """Test timeouts of connections from the DataClient to the server.
-
-        The CSC should fault when the DataClient loses its connection to the
-        server and the DataClient should reconnect when the CSC is set to
-        ENABLED again.
-        """
-        # Start the MockServer for manual control.
-        await self.start_socket_server()
-        async with self.make_csc(
-            initial_state=salobj.State.ENABLED,
-            config_dir=TEST_CONFIG_DIR,
-            simulation_mode=0,
-            override="test_one_temp_sensor.yaml",
-        ):
-            await self.assert_next_summary_state(
-                salobj.State.ENABLED, timeout=STATE_TIMEOUT
-            )
-
-            await self.assert_next_sample(topic=self.remote.evt_errorCode, errorCode=0)
-            assert len(self.csc.data_clients) == 1
-
-            # Stop the MockServer.
-            await self.stop_socket_server()
-            await self.assert_next_summary_state(
-                salobj.State.FAULT, timeout=STATE_TIMEOUT
-            )
-
-            await salobj.set_summary_state(
-                remote=self.remote, state=salobj.State.STANDBY
-            )
-            await self.assert_next_summary_state(
-                salobj.State.STANDBY, timeout=STATE_TIMEOUT
-            )
-
-            # Start the MockServer again.
-            await self.start_socket_server()
 
             await salobj.set_summary_state(
                 remote=self.remote, state=salobj.State.ENABLED
@@ -772,39 +651,29 @@ class CscTestCase(salobj.BaseCscTestCase, unittest.IsolatedAsyncioTestCase):
             await self.assert_next_summary_state(
                 salobj.State.DISABLED, timeout=STATE_TIMEOUT
             )
-            # Patch the "connect" method so we can count how often it was
-            # called.
-            with mock.patch.object(
-                common.data_client.Young32400WeatherStationDataClient,
-                "connect",
-                wraps=self.csc.data_clients[0].connect,
-            ) as connect_mock:
-                assert len(self.csc.data_clients) == 1
-                assert self.csc.data_clients[0] is not None
-                assert self.csc.data_clients[0].mock_data_server is None
-                self.csc.data_clients[0].do_timeout = True
-                # Not started yet so "connect" wasn't called yet.
-                connect_mock.assert_not_called()
+            assert len(self.csc.data_clients) == 1
+            assert self.csc.data_clients[0] is not None
+            assert self.csc.data_clients[0].mock_data_server is None
+            self.csc.data_clients[0].do_timeout = True
 
-                await salobj.set_summary_state(
-                    remote=self.remote, state=salobj.State.ENABLED
-                )
-                await self.assert_next_summary_state(
-                    salobj.State.ENABLED, timeout=STATE_TIMEOUT
-                )
-                assert len(self.csc.data_clients) == 1
-                assert self.csc.data_clients[0].mock_data_server is not None
-                assert self.csc.data_clients[0].mock_data_server.connected
-                # Started with a short timeout so "connect" was called several
-                # times.
-                connect_mock.assert_called()
+            await salobj.set_summary_state(
+                remote=self.remote, state=salobj.State.ENABLED
+            )
+            await self.assert_next_summary_state(
+                salobj.State.ENABLED, timeout=STATE_TIMEOUT
+            )
+            assert len(self.csc.data_clients) == 1
+            assert self.csc.data_clients[0].mock_data_server is not None
+            assert not self.csc.data_clients[0].mock_data_server.connected
 
-                # Give the client time to recover from the timeout.
-                await asyncio.sleep(2.0)
-                # Recovered from the timeout such that "connect" should be
-                # called at least 5 times, since the data client will attempt
-                # to reconnect 5 times in case of a timeout.
-                assert len(connect_mock.call_args_list) >= 5
+            # Give the client time to recover from the timeout.
+            await asyncio.sleep(2.0)
+
+            # The CSC should go to FAULT state because of the long interval
+            # between reading consecutive data.
+            await self.assert_next_summary_state(
+                salobj.State.FAULT, timeout=STATE_TIMEOUT
+            )
 
     async def test_spectrum_analyzer_data_client_loses_connection(self) -> None:
         """Test timeouts of connections from the DataClient to the server.
@@ -836,7 +705,7 @@ class CscTestCase(salobj.BaseCscTestCase, unittest.IsolatedAsyncioTestCase):
             )
             assert len(self.csc.data_clients) == 1
             assert self.csc.data_clients[0].mock_data_server is not None
-            assert self.csc.data_clients[0].mock_data_server.connected
+            assert not self.csc.data_clients[0].mock_data_server.connected
 
             # The CSC should go to FAULT state because of the long interval
             # between reading consecutive data.
